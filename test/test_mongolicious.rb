12aac5fa0360cd4f0525565d347ddf9ffb1d41c4
Marc Boeker <marc@at6.net>
2011-02-17 09:49:22 +0100

    Removed double requirement of mongolicous/backup.

--- test/test_mongolicious.rb
+++ test/test_mongolicious.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class TestMongolicious < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "work" do
+    
   end
 end
